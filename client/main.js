424def05b2aa53a61aefd886f9554584d87d3fc1
MattJarman <mattjarman22@outlook.com>
2018-04-22 17:22:03 +0100

    updated files

--- client/main.js
+++ client/main.js
@@ -1,30 +1,85 @@
 import { Template } from 'meteor/templating';
-import { Notes } from '../lib/collections.js';
-import { Personnel } from '../lib/collections.js';
+import { Session } from 'meteor/session';
 import { OnCall } from '../lib/collections.js';
 import { Deployed } from '../lib/collections.js';
+import { Pending } from '../lib/collections.js';
+import { Operational } from '../lib/collections.js';
 import { Accounts } from 'meteor/accounts-base';
 import dragula from 'dragula';
+import './main.html';
 
-var lastBoard = null;
-
+Session.set('isTeam', true);
+var formData = 1;
 
 //Accounts config
 Accounts.ui.config({
   passwordSignupFields:'USERNAME_ONLY'
 });
 
-import './main.html';
-
+//Body helpers
 Template.body.helpers({
   onCall(){
     return OnCall.find({})
+  },
+  deployed(){
+    return Deployed.find({})
+  },
+  pending(){
+    return Pending.find({})
+  },
+  operational(){
+    return Operational.find({})
   }
 });
 
-Template.body.helpers({
-  deployed(){
-    return Deployed.find({})
+//Add helpers
+Template.add.helpers({
+  isPersonnel: function(){
+    return Session.get('isPersonnel');
+  },
+  isTeam: function(){
+    return Session.get('isTeam');
+  },
+  isBoat: function(){
+    return Session.get('isBoat');
+  }
+});
+
+//Enables select on form
+Template.team.onRendered(function(){
+  this.$('select').material_select();
+});
+
+Template.personnel.onRendered(function(){
+  this.$('select').material_select();
+});
+
+Template.boat.onRendered(function(){
+  this.$('select').material_select();
+});
+
+//Add event
+Template.add.events({
+  'change select': function(el, e) {
+    event.preventDefault();
+    var e = document.getElementById("cardType");
+    formData = e.options[e.selectedIndex].value;
+
+    if(formData == 1) {
+      Session.set('isTeam', true);
+      Session.set('isPersonnel', false);
+      Session.set('isBoat', false);
+    }
+    else if(formData == 2) {
+      Session.set('isTeam', false);
+      Session.set('isPersonnel', true);
+      Session.set('isBoat', false);
+    }
+    else if(formData == 3) {
+      Session.set('isTeam', false);
+      Session.set('isPersonnel', false);
+      Session.set('isBoat', true);
+    }
   }
 });
 
@@ -32,49 +87,54 @@ Template.add.events({
   'submit .add-form': function(el, e){
     event.preventDefault();
 
+    //FormData is the card choice e.g. 1 = team card
+    if(formData == 1) {
+    }
+    else if(formData == 2){
     const target = event.target;
     const cardType = 'personnel';
-    const agency = target.agency.value;
-    const name = target.name.value;
-    const incident_role = target.incidentRole.value;
-    const mobile_no = target.mobileNo.value;
-    const callsign = target.callsign.value;
-    const request_no = target.requestNo.value;
-    const checkIn = target.checkIn.value;
-    const departure_point = target.departurePoint.value;
-    const travel_method = target.travelMethod.value;
-    const date_time = target.dateTime.value;
-    const date_time1 = target.dateTime1.value;
-    const arrival_location = target.arrivalLocation.value;
-    const eta = target.eta.value;
-    const unknown = target.unknown.value;
-    const incident_location = target.incidentLocation.value;
-    const time = target.time.value;
-    const status = target.status.value;
-    const notes = target.notes.value;
+    const agency = target.agencyP.value;
+    const name = target.nameP.value;
+    const incident_role = target.incidentRoleP.value;
+    const mobile_no = target.mobileNoP.value;
+    const callsign = target.callsignP.value;
+    const request_no = target.requestNoP.value;
+    const checkIn = target.checkInP.value;
+    const departure_point = target.departurePointP.value;
+    const travel_method = target.travelMethodP.value;
+    const date_time = target.dateTimeP.value;
+    const date_time1 = target.dateTime1P.value;
+    const arrival_location = target.arrivalLocationP.value;
+    const eta = target.etaP.value;
+    const unknown = target.unknownP.value;
+    const incident_location = target.incidentLocationP.value;
+    const time = target.timeP.value;
+    const status = target.statusP.value;
+    const notes = target.notesP.value;
 
     OnCall.insert({
       cardType,
       agency,
       name,
-      incidentRole,
-      mobileNo,
+      incident_role,
+      mobile_no,
       callsign,
-      requestNo,
+      request_no,
       checkIn,
-      departurePoint,
-      travelMethod,
-      dateTime,
-      dateTime1,
-      arrivalLocation,
+      departure_point,
+      travel_method,
+      date_time,
+      date_time1,
+      arrival_location,
       eta,
       unknown,
-      incidentLocation,
+      incident_location,
       time,
       status,
       notes,
       createdAt: new Date()
-    });
+      });
+    }
 
     $("#personnel-form").trigger('reset');
     $('.modal').modal('close');
@@ -82,7 +142,7 @@ Template.add.events({
 });
 
 
-Template.body.onRendered(function() {
+Template.body.onRendered(function(el, e) {
   var drake = dragula([document.querySelector('#b1'), document.querySelector('#b2'),
   document.querySelector('#b3'), document.querySelector('#b4')]);
 
@@ -96,12 +156,76 @@ Template.body.onRendered(function() {
           Deployed.remove(el.id);
         }
       )
+      Pending.find({ _id: el.id}).forEach(
+        function(doc){
+          OnCall.insert(doc);
+          Pending.remove(el.id);
+        }
+      )
+      Operational.find({ _id: el.id}).forEach(
+        function(doc){
+          OnCall.insert(doc);
+          Operational.remove(el.id);
+        }
+      )
     }
     else if(board === 'b2') {
       OnCall.find({ _id: el.id}).forEach(
         function(doc){
           Deployed.insert(doc);
           OnCall.remove(el.id);
+        }
+      )
+      Pending.find({ _id: el.id}).forEach(
+        function(doc){
+          Deployed.insert(doc);
+          Pending.remove(el.id);
+        }
+      )
+      Operational.find({ _id: el.id}).forEach(
+        function(doc){
+          Deployed.insert(doc);
+          Operational.remove(el.id);
+        }
+      )
+    }
+    else if(board === 'b3') {
+      OnCall.find({ _id: el.id}).forEach(
+        function(doc){
+          Pending.insert(doc);
+          OnCall.remove(el.id);
+        }
+      )
+      Deployed.find({ _id: el.id}).forEach(
+        function(doc){
+          Pending.insert(doc);
+          Deployed.remove(el.id);
+        }
+      )
+      Operational.find({ _id: el.id}).forEach(
+        function(doc){
+          Pending.insert(doc);
+          Operational.remove(el.id);
+        }
+      )
+    }
+    else if(board === 'b4') {
+      OnCall.find({ _id: el.id}).forEach(
+        function(doc){
+          Operational.insert(doc);
+          OnCall.remove(el.id);
+        }
+      )
+      Deployed.find({ _id: el.id}).forEach(
+        function(doc){
+          Operational.insert(doc);
+          Deployed.remove(el.id);
+        }
+      )
+      Pending.find({ _id: el.id}).forEach(
+        function(doc){
+          Operational.insert(doc);
+          Pending.remove(el.id);
         }
       )
     }
@@ -115,14 +239,11 @@ $(document).ready(function(){
 
   Template.card.events({
     'click .delete-card': function(){
-  	  if(confirm("Do you want to delete this card?"))
-  	  {
-
+  	  if(confirm("Do you want to delete this card?")){
       OnCall.remove(this._id);
       Deployed.remove(this._id);
-  	  }
-  	  else{
-
+      Pending.remove(this._id);
+      Operational.remove(this._id);
   	  }
     }
   });
